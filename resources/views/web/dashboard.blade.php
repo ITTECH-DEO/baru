ffb5b81eda49dd69b2c420963738516626006966
dafaathaillah <dafabintang76@gmail.com>
2022-12-29 20:46:12 +0800

    update card car

--- resources/views/web/dashboard.blade.php
+++ resources/views/web/dashboard.blade.php
@@ -23,50 +23,43 @@
                 @foreach ($cars as $car)
                     @php $data = $modelT->cek_booked($car->id); @endphp
                     @if ($car->status_id == 1)
-                        <div class="col-md-6">
-                            <div class="">
-                                <img class="zoom_01 round" style="width: 520px;height: 360px;" src="{{ $car->img_car }}"
-                                    data-zoom-image="{{ $car->img_car }}" />
-                                {{-- <div class="down-content">
-                    <a href="{{url('web/car-details.html')}}"><h4>{{$car->name_car}}</h4></a>
-
-                    <h6><small><b> Price :</b></small> {{number_format($car->day_price)}}</h6>
-
-                    <p>{{$car->model}} &nbsp;/&nbsp; {{$car->power}} &nbsp;/&nbsp; {{$car->fisrt_registartion}} &nbsp;/&nbsp; {{$car->fuel}}</p>
-
-                    <small>
-                      <strong title="Author"><i class="fa fa-dashboard"></i> {{$car->millage}}</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                      <strong title="Author"><i class="fa fa-cube"></i> {{$car->engine_size}}</strong>&nbsp;&nbsp;&nbsp;&nbsp;
-                      <strong title="Views"><i class="fa fa-cog"></i> {{$car->tyoe_car}}</strong>
-                    </small>
-                  </div> --}}
-                                {{-- <br><br>
-                  @if ($data)
-                  @if ($data->status_transaction == 'process')
-                   <p align="right"><a href="#" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#Booked">Booked</a></p>
-                  @elseif($data->status_transaction=='agree')
-                    <p align="right"><a href="#" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#Sold">Sold</a></p>
-                  @else
-                   <p align="right"><a href="{{url('car_detail/'.$car->id)}}" class="btn btn-info btn-sm">Detail</a></p>
-                  @endif
-                  @else
-                   <p align="right"><a href="{{url('car_detail/'.$car->id)}}" class="btn btn-info btn-sm">Detail</a></p>
-                  @endif
-                </div> --}}
+                    <div class="card round">
+                        <img class="zoom_01 round" style="width: 520px;height: 360px;" src="{{ $car->img_car }}"
+                            data-zoom-image="{{ $car->img_car }}" />
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6 pull-left">
+                                    <b>Nama Mobil:</b><br>
+                                    <b>Type Mobil:</b><br>
+                                    <b>Vendor Mobil:</b>
+                                </div>
+                                <div class="col-md-6 pull-right">
+                                    <b>{{ $car->name_car }}</b><br>
+                                    <b>{{ $car->type_car }}</b><br>
+                                    <b>{{ $car->vendor->name_vendor }}</b>
+                                </div>
                             </div>
-
                         </div>
-                    @else
-                    @endif
-                @endforeach
-                {{-- <div class="col-md-12">
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="pull-left col-md-6">
+                                    <b>RP. {{ $car->day_price }}</b>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        @else
+            @endif
+            @endforeach
+            {{-- <div class="col-md-12">
                 <ul class="pages">
                  {{$cars->links()}}
                 </ul>
               </div> --}}
-            </div>
         </div>
     </div>
+</div>
 </div>
 </div>
 {{-- <div class="modal fade" id="Booked" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
