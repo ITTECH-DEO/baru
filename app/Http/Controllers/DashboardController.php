b3a894d741f66652a2ce1128112ea0acfe029cbd
mbahgembrong <mbahgembrong.sos@gmail.com>
2023-01-04 02:37:19 +0700

    fix dashboard respon car 2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index(){
-        $cars = Car::with('vendor')->orderBy('updated_at', 'DESC')->take(2)->get();
+        $cars = Car::with('vendor')->orderBy('updated_at', 'DESC')->get();
         return view('web.dashboard',compact('cars'));
     }
 }
