05735c606ab971d68d085f73f7ed091c25c6212b
mbahgembrong <mbahgembrong.sos@gmail.com>
2023-01-05 00:09:35 +0700

    fix menu footer position

--- resources/views/web/footer.blade.php
+++ resources/views/web/footer.blade.php
@@ -1,158 +1,190 @@
  <html>
-  <body>
 
-  <head>
-  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+ <body>
 
-  <link rel="stylesheet" href="{{url('web/assets/css/ionicons.min.css')}}">
-  <link rel="stylesheet" href="{{url('web/assets/css/style1.css')}}">
-  </head>
+     <head>
+         <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+         <link rel="stylesheet" href="{{ url('web/assets/css/ionicons.min.css') }}">
+         <link rel="stylesheet" href="{{ url('web/assets/css/style1.css') }}">
+     </head>
+     <style>
+         .text {
+             position: inherit;
+             text-align: left;
+         }
+     </style>
+     <footer class="footer-09">
+         <div class="container">
+             <div class="row">
+                 <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
+                     <h2 class="footer-heading d-flex">
+                         <span class="icon d-flex align-items-center justify-content-center"><i
+                                 class="ion-logo-ionic"></i></span>
+                         About
+                     </h2>
+                     <div class="block-23 mb-3">
+                         <ul>
+                             <li>
+                                 <span class="icon ion-ios-pin"></span>
+                                 <span class="text">203 Fake St. Mountain View,San Francisco, California, USA</span>
+                             </li>
+                             <li>
+                                 <a href="#"><span class="icon ion-ios-call"></span>
+                                     <span class="text">+2 392 3929 210</span></a>
+                             </li>
+                             <li><a href="#">
+                                     <span class="icon ion-ios-send"></span>
+                                     <span class="text">info@yourdomain.com</span>
+                                 </a>
+                             </li>
+                         </ul>
+                     </div>
+                     <form action="#" class="subscribe-form">
+                         <div class="form-group d-flex">
+                             <input type="text" class="form-control rounded-left" placeholder="Enter email address">
+                             <button type="submit" class="form-control submit rounded-right"><span
+                                     class="sr-only">Submit</span><i class="ion-ios-send"></i></button>
+                         </div>
+                     </form>
+                 </div>
+                 <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
+                     <h2 class="footer-heading d-flex align-items-center"><span
+                             class="icon d-flex align-items-center justify-content-center"><i
+                                 class="ion-ios-list-box"></i></span>Latest News</h2>
+                     <div class="block-21 mb-4 d-flex">
+                         <a class="img mr-4 rounded" style="background-image: url(images/image_1.jpg);"></a>
+                         <div class="text">
+                             <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control
+                                     about</a></h3>
+                             <div class="meta">
+                                 <div><a href="#"><span class="icon-calendar"></span> Oct. 16, 2019</a></div>
+                                 <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                                 <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="block-21 mb-4 d-flex">
+                         <a class="img mr-4 rounded" style="background-image: url(images/image_2.jpg);"></a>
+                         <div class="text">
+                             <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control
+                                     about</a></h3>
+                             <div class="meta">
+                                 <div><a href="#"><span class="icon-calendar"></span> Oct. 16, 2019</a></div>
+                                 <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                                 <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
+                     <h2 class="footer-heading d-flex align-items-center"><span
+                             class="icon d-flex align-items-center justify-content-center"><i
+                                 class="ion-ios-document"></i></span>Information</h2>
+                     <ul class="list-unstyled">
+                         <li><a href="#" class="py-1 d-block">About</a></li>
+                         <li><a href="#" class="py-1 d-block">Products</a></li>
+                         <li><a href="#" class="py-1 d-block">Blog</a></li>
+                         <li><a href="#" class="py-1 d-block">Contact</a></li>
+                         <li><a href="#" class="py-1 d-block">Help &amp; Support</a></li>
+                     </ul>
+                 </div>
+                 <div class="col-md-6 col-lg-3 mb-md-0 mb-4">
+                     <h2 class="footer-heading d-flex align-items-center"><span
+                             class="icon d-flex align-items-center justify-content-center"><i
+                                 class="ion-logo-instagram"></i></span>Instagram</h2>
+                     <div class="block-24">
+                         <div class="row no-gutters">
+                             <div class="col-4 col-md-4 p-1">
+                                 <a href="#" class="img rounded"
+                                     style="background-image: url(images/insta-1.jpg);"></a>
+                             </div>
+                             <div class="col-4 col-md-4 p-1">
+                                 <a href="#" class="img rounded"
+                                     style="background-image: url(images/insta-2.jpg);"></a>
+                             </div>
+                             <div class="col-4 col-md-4 p-1">
+                                 <a href="#" class="img rounded"
+                                     style="background-image: url(images/insta-3.jpg);"></a>
+                             </div>
+                             <div class="col-4 col-md-4 p-1">
+                                 <a href="#" class="img rounded"
+                                     style="background-image: url(images/insta-4.jpg);"></a>
+                             </div>
+                             <div class="col-4 col-md-4 p-1">
+                                 <a href="#" class="img rounded"
+                                     style="background-image: url(images/insta-5.jpg);"></a>
+                             </div>
+                             <div class="col-4 col-md-4 p-1">
+                                 <a href="#" class="img rounded"
+                                     style="background-image: url(images/insta-6.jpg);"></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="row mt-5 pt-4 border-top">
+                 <div class="col-md-6 col-lg-8">
+                     <p class="copyright">
+                         Copyright &copy;
+                         <script>
+                             document.write(new Date().getFullYear());
+                         </script> All rights reserved.
+                     </p>
+                 </div>
+                 <div class="col-md-6 col-lg-4 text-md-right">
+                     <p class="copyright">This template is made with <i class="ion-ios-heart" aria-hidden="true"></i>
+                         by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
+                     </p>
+                 </div>
+             </div>
+         </div>
+     </footer>
 
 
-  <footer class="footer-09">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-lg-3 mb-md-0 mb-4">
-						<h2 class="footer-heading d-flex">
-							<span class="icon d-flex align-items-center justify-content-center"><i class="ion-logo-ionic"></i></span>
-							About
-						</h2>
-						<div class="block-23 mb-3">
-              <ul>
-                <li><span class="icon ion-ios-pin"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                <li><a href="#"><span class="icon ion-ios-call"></span><span class="text">+2 392 3929 210</span></a></li>
-                <li><a href="#"><span class="icon ion-ios-send"></span><span class="text">info@yourdomain.com</span></a></li>
-              </ul>
-            </div>
-            <form action="#" class="subscribe-form">
-              <div class="form-group d-flex">
-                <input type="text" class="form-control rounded-left" placeholder="Enter email address">
-                <button type="submit" class="form-control submit rounded-right"><span class="sr-only">Submit</span><i class="ion-ios-send"></i></button>
-              </div>
-            </form>
-					</div>
-					<div class="col-md-6 col-lg-3 mb-md-0 mb-4">
-						<h2 class="footer-heading d-flex align-items-center"><span class="icon d-flex align-items-center justify-content-center"><i class="ion-ios-list-box"></i></span>Latest News</h2>
-						<div class="block-21 mb-4 d-flex">
-              <a class="img mr-4 rounded" style="background-image: url(images/image_1.jpg);"></a>
-              <div class="text">
-                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                <div class="meta">
-                  <div><a href="#"><span class="icon-calendar"></span> Oct. 16, 2019</a></div>
-                  <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                  <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                </div>
-              </div>
-            </div>
-            <div class="block-21 mb-4 d-flex">
-              <a class="img mr-4 rounded" style="background-image: url(images/image_2.jpg);"></a>
-              <div class="text">
-                <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
-                <div class="meta">
-                  <div><a href="#"><span class="icon-calendar"></span> Oct. 16, 2019</a></div>
-                  <div><a href="#"><span class="icon-person"></span> Admin</a></div>
-                  <div><a href="#"><span class="icon-chat"></span> 19</a></div>
-                </div>
-              </div>
-            </div>
-					</div>
-					<div class="col-md-6 col-lg-3 mb-md-0 mb-4">
-						<h2 class="footer-heading d-flex align-items-center"><span class="icon d-flex align-items-center justify-content-center"><i class="ion-ios-document"></i></span>Information</h2>
-						<ul class="list-unstyled">
-	            <li><a href="#" class="py-1 d-block">About</a></li>
-	            <li><a href="#" class="py-1 d-block">Products</a></li>
-	            <li><a href="#" class="py-1 d-block">Blog</a></li>
-	            <li><a href="#" class="py-1 d-block">Contact</a></li>
-	            <li><a href="#" class="py-1 d-block">Help &amp; Support</a></li>
-	          </ul>
-					</div>
-					<div class="col-md-6 col-lg-3 mb-md-0 mb-4">
-						<h2 class="footer-heading d-flex align-items-center"><span class="icon d-flex align-items-center justify-content-center"><i class="ion-logo-instagram"></i></span>Instagram</h2>
-						<div class="block-24">
-							<div class="row no-gutters">
-								<div class="col-4 col-md-4 p-1">
-									<a href="#" class="img rounded" style="background-image: url(images/insta-1.jpg);"></a>
-								</div>
-								<div class="col-4 col-md-4 p-1">
-									<a href="#" class="img rounded" style="background-image: url(images/insta-2.jpg);"></a>
-								</div>
-								<div class="col-4 col-md-4 p-1">
-									<a href="#" class="img rounded" style="background-image: url(images/insta-3.jpg);"></a>
-								</div>
-								<div class="col-4 col-md-4 p-1">
-									<a href="#" class="img rounded" style="background-image: url(images/insta-4.jpg);"></a>
-								</div>
-								<div class="col-4 col-md-4 p-1">
-									<a href="#" class="img rounded" style="background-image: url(images/insta-5.jpg);"></a>
-								</div>
-								<div class="col-4 col-md-4 p-1">
-									<a href="#" class="img rounded" style="background-image: url(images/insta-6.jpg);"></a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row mt-5 pt-4 border-top">
-          <div class="col-md-6 col-lg-8">
-            <p class="copyright">
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved.</p>
-          </div>
-          <div class="col-md-6 col-lg-4 text-md-right">
-          	<p class="copyright">This template is made with <i class="ion-ios-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-  </p>
-          </div>
-        </div>
-			</div>
-		</footer>
+     <!-- Bootstrap core JavaScript -->
+     <script src="{{ url('web/vendor/jquery/jquery.min.js') }}"></script>
+     {{-- <script src="{{url('dashboard/js/jquery-1.8.3.min.js')}}"></script> --}}
+     <script src="{{ url('dashboard/js/jquery.elevatezoom.js') }}"></script>
+     <!-- <script src="{{ url('web/assets/js/jquery.min.js') }}"></script> -->
+     <script src="{{ url('web/assets/js/popper.js') }}"></script>
+     <script src="{{ url('web/assets/js/bootstrap.min.js') }}"></script>
+     {{-- <script src="{{url('web/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script> --}}
+     <script src="{{ url('web/assets/js/main1.js') }}"></script>
+     {{-- <script src="{{url('dashboard/js/google-map.js')}}"></script> --}}
+     {{-- <script src="{{url('dashboard/js/jquery.min.js')}}"></script> --}}
+     {{-- <script src="{{url('dashboard/js/main.js')}}"></script> --}}
+     {{-- <script src="{{url('dashboard/js/popper.js')}}"></script> --}}
+     {{-- <script src="{{url('dashboard/js/bootstrap.min.js')}}"></script> --}}
+     {{-- <script src="{{url('dashboard/js/jquery.min.js')}}"></script> --}}
+     {{-- <script src="{{url('dashboard/js/validate.min.js')}}"></script> --}}
+     <script src="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+     {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> --}}
+     @stack('dashboardfooter')
+     <!-- Additional Scripts -->
+     <script src="{{ url('web/assets/js/custom.js') }}"></script>
+     <script src="{{ url('web/assets/js/owl.js') }}"></script>
+     <script type="text/javascript">
+         function submitForm() {
+             $('#muter_beh').show();
+             $('#submit_form').hide();
+             setTimeout(function() {
+                 window.location.href = "{{ url('/') }}";
+             }, 7000);
+         }
+     </script>
+     <script>
+         $(".zoom_01").elevateZoom({
+             easing: true
+         });
+         $(document).ready(function() {
+             $('#btn1').on("click", function(e) {
+                 $('#myImg').toggle('slow');
+                 $('#myImg2').toggle('slow');
+                 $('#myImg3').toggle('slow');
 
+             });
+         });
+     </script>
+ </body>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="{{url('web/vendor/jquery/jquery.min.js')}}"></script>
-    {{-- <script src="{{url('dashboard/js/jquery-1.8.3.min.js')}}"></script> --}}
-    <script src="{{url('dashboard/js/jquery.elevatezoom.js')}}"></script>
-    <!-- <script src="{{url('web/assets/js/jquery.min.js')}}"></script> -->
-    <script src="{{url('web/assets/js/popper.js')}}"></script>
-    <script src="{{url('web/assets/js/bootstrap.min.js')}}"></script>
-    {{-- <script src="{{url('web/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script> --}}
-    <script src="{{url('web/assets/js/main1.js')}}"></script>
-    {{-- <script src="{{url('dashboard/js/google-map.js')}}"></script> --}}
-    {{-- <script src="{{url('dashboard/js/jquery.min.js')}}"></script> --}}
-    {{-- <script src="{{url('dashboard/js/main.js')}}"></script> --}}
-    {{-- <script src="{{url('dashboard/js/popper.js')}}"></script> --}}
-    {{-- <script src="{{url('dashboard/js/bootstrap.min.js')}}"></script> --}}
-    {{-- <script src="{{url('dashboard/js/jquery.min.js')}}"></script> --}}
-    {{-- <script src="{{url('dashboard/js/validate.min.js')}}"></script> --}}
- <script src="https://cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
-  {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>--}}
-    @stack('dashboardfooter')
-    <!-- Additional Scripts -->
-    <script src="{{url('web/assets/js/custom.js')}}"></script>
-    <script src="{{url('web/assets/js/owl.js')}}"></script>
-    <script type="text/javascript">
-
-      function submitForm()
-      {
-          $('#muter_beh').show();
-          $('#submit_form').hide();
-          setTimeout(function() {
-           window.location.href= "{{url('/')}}";
-        }, 7000);
-      }
-      </script>
-    <script>
-
-      $(".zoom_01").elevateZoom({
-        easing : true
-      });
-      $(document).ready(function(){
-  $('#btn1').on("click",function(e){
-   $('#myImg').toggle('slow');
-   $('#myImg2').toggle('slow');
-   $('#myImg3').toggle('slow');
-
-  });
-  });
-      </script>
-  </body>
-
-</html>
+ </html>
