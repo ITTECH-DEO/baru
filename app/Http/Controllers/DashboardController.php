0eaf97f5b4ee8e6d578e8eaee2a47c97b2321ab8
mbahgembrong <mbahgembrong.sos@gmail.com>
2023-02-03 03:23:08 +0700

    fix return search filtering dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,52 +5,58 @@ namespace App\Http\Controllers;
 use App\Models\Banner;
 use App\Models\Car;
 use Illuminate\Http\Request;
+use JetBrains\PhpStorm\Internal\ReturnTypeContract;
 
 class DashboardController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $banners = Banner::all();
         $cars = Car::with('vendor')->orderBy('updated_at', 'DESC')->take(6)->get();
-        $manual = Car::where("type_car","=","Manual")->with('vendor')->orderBy('updated_at', 'DESC')->take(6)->get();
-        $matic = Car::where("type_car","=","Matic")->with('vendor')->orderBy('updated_at', 'DESC')->take(6)->get();
+        $manual = Car::where("type_car", "=", "Manual")->with('vendor')->orderBy('updated_at', 'DESC')->take(6)->get();
+        $matic = Car::where("type_car", "=", "Matic")->with('vendor')->orderBy('updated_at', 'DESC')->take(6)->get();
         $banner = Banner::orderBy('updated_at', 'DESC')->latest()->get();
 
         // return $manual;
-        return view('web.dashboard',compact('cars','manual','matic','banner'));
+        return view('web.dashboard', compact('cars', 'manual', 'matic', 'banner'));
     }
 
-    public function mobilMatic(){
-        $cars = Car::where("type_car","=","Matic")->get();
-        return view("web.matic",["cars"=>$cars]);
+    public function mobilMatic()
+    {
+        $cars = Car::where("type_car", "=", "Matic")->get();
+        return view("web.matic", ["cars" => $cars]);
     }
 
-    public function mobilManual(){
-        $cars = Car::where("type_car","=","Manual")->get();
-        return view("web.manual",["cars"=>$cars]);
+    public function mobilManual()
+    {
+        $cars = Car::where("type_car", "=", "Manual")->get();
+        return view("web.manual", ["cars" => $cars]);
     }
 
-    public function allCars(){
+    public function allCars()
+    {
         $cars = Car::all();
-        return view("web.allcars",["cars"=>$cars]);
+        return view("web.allcars", ["cars" => $cars]);
     }
 
     public function search_products(Request $request)
     {
-        $all_products = Car::whereBetween('day_price',[$request->left_value, $request->right_value])->get();
-        return view('search_result',compact('all_products'))->render();
+
+        $all_products = Car::whereBetween('day_price', [(int)$request->left_value, (int) $request->right_value])->get();
+        return response()->json($all_products);
+        // return view('search_result', compact('all_products'))->render();
     }
 
     public function sort_by(Request $request)
     {
-        if($request->sort_by == 'lowest_price'){
-            $all_products = Car::orderBy('day_price','asc')->get();
+        if ($request->sort_by == 'lowest_price') {
+            $all_products = Car::orderBy('day_price', 'asc')->get();
         }
-        if($request->sort_by == 'highest_price'){
-            $all_products = Car::orderBy('day_price','desc')->get();
-        }
-        return view('search_result',compact('all_products'))->render();
+        if ($request->sort_by == 'highest_price') {
+            $all_products = Car::orderBy('day_price', 'desc')->get();
 
+        }
+        return response()->json($all_products);
+        // return view('search_result', compact('all_products'))->render();
     }
-
-
 }
